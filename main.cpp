b5b6cbf094722eb60db26296691f87828b468194
tuanvan <108537748+DavidVan1@users.noreply.github.com>
2025-03-09 14:05:04 +0100

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Applicants.h"
-
-int main() {
-  BachelorApplicants bachelorApplicants;
-  bachelorApplicants.loadBachelorApplicants("bachelorapplicants.txt");
-
-  cout << "hello" << endl;
-  return 0;
-}
